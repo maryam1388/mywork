da6c176d69921b09bba5beebd0517ed178af00c8
mkayf <muhammadkaif150@gmail.com>
2025-08-01 16:10:16 +0500

    first commit

---
+++ work.js
@@ -0,0 +1,16 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter a single character: ', input => {
+  let char = input.toLowerCase(); // convert to lowercase to handle capital letters too
+
+  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+    console.log("It is vowel");
+  } else {
+    console.log("It is consonant");
+  }
+
+  readline.close();
+});
